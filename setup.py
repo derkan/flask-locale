af1ccdd2173ccf64221b493fef9650709da676e5
erkan <erkan@eurotel.com.tr>
2015-10-09 17:46:36 +0300

    First Release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Locale',
-    version='0.1',
+    version='0.2',
     url='http://github.com/derkan/flask-locale',
     license='BSD',
     author='Erkan Durmus',
