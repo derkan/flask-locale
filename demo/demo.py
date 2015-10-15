a5f9ddc202e985ae1ae425e43c64935254523dbf
erkan <erkan@eurotel.com.tr>
2015-10-15 14:09:07 +0300

    Locale detection improvement.

--- demo/demo.py
+++ demo/demo.py
@@ -1,25 +1,32 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, request, render_template, g
+from flask import Flask, request, render_template, g, session, redirect, current_app
 from flask_locale import Locale, _
 
 app = Flask(__name__)
-app.config['DEFAULT_LOCALE'] = 'tr_TR'
+# DEFAULT_LOCALE is the language used for keys ins translation files:
+app.config['DEFAULT_LOCALE'] = 'en_US'
 app.config['LOCALE_PATH'] = 'translations'
+app.config['SECRET_KEY'] = 'translations****'
 
 locale = Locale(app)
 
 
 @locale.localeselector
 def get_locale():
-    # if a user is logged in, use the locale from the user settings
-    user = getattr(g, 'user', None)
-    if user is not None:
-        return user.locale
+    # if a user is logged in, use the locale from the session
+    # define a default value instead of None to set it to specific locale if not setting is found.
+    locale_code = session.get('locale', None)
+    if locale_code is not None:
+        current_app.logger.info("Locale is: %s" % locale_code)
+        return locale_code
+
     # otherwise try to guess the language from the user accept
     # header the browser transmits.  We support tr/fr/en in this
     # example.  The best match wins.
-    return request.accept_languages.best_match(['tr_TR', 'fr_FR', 'en_US'])
+    locale_code = request.accept_languages.best_match(['tr_TR', 'fr_FR', 'en_US'])
+    current_app.logger.info("Locale match: %s" % locale_code)
+    return locale_code
 
 
 @app.route("/")
@@ -28,6 +35,13 @@ def index():
     py_translated = _('Hello')
     # How we do translation in template:
     return render_template('locale.html', name='Erkan', py_translated=py_translated)
+
+
+@app.route("/locale")
+def change_locale():
+    new_locale = request.args.get('locale', None)
+    session['locale'] = new_locale
+    return redirect('/')
 
 
 if __name__ == '__main__':
